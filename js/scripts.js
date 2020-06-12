faeda7703fc57dd8b269bd4bde4daf8ddca1760f
Kate Skorija <kateskorija@gmail.com>
2020-06-12 12:03:03 -0500

    complete third spec; add else statement to replace 2 with Boop

--- js/scripts.js
+++ js/scripts.js
@@ -27,14 +27,16 @@ function replaceDigits (inputArray) {
   for (index = 0; index < inputArray.length; index++) {
     if (inputArray[index] === 1) {
       inputArray.splice(index, 1, "Beep!");
+    } else if (inputArray[index] === 2) {
+      inputArray.splice(index, 1, "Boop!");
     }
   }
   return inputArray
 }
 
 
-// Program will return a range of numbers from 0 to the users inputted number
-// Program will replace all 1's with "Beep!"
+// Program will return a range of numbers from 0 to the users inputted number DONE!
+// Program will replace all 1's with "Beep!" DONE!
 // Program will replace all 2's with "Boop!"
 // Program will replace all 3's with "Won't you be my neighbor?"
 // Program will apply the rule for 3's over the rule for 1's or 2's
